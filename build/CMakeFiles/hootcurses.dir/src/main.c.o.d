caf7bd30bdb39aae19bb38d0352f983b68f75cd1
Jann Bajade <jannalexandrei.campos@gmail.com>
2026-02-25 13:33:49 +0800

    uncached- sana

--- build/CMakeFiles/hootcurses.dir/src/main.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/hootcurses.dir/src/main.c.o: \
- /home/jannbajade/dev/c/hootcurses/src/main.c /usr/include/stdc-predef.h \
- /home/jannbajade/dev/c/hootcurses/src/hootcurses.h \
- /home/jannbajade/dev/c/hootcurses/src/types.h \
- /home/jannbajade/dev/c/hootcurses/libs/resizable_array/include/resizable_array.h \
- /home/jannbajade/dev/c/hootcurses/src/screen_surface.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdbool.h
